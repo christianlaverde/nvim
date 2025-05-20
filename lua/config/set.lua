910a7cd1f70bfc4d977e1c49a853af143e38b55f
Christian LaVerde <chrislaverde1@gmail.com>
2025-05-19 21:20:42 -0500

    Add set for system clipboard

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -3,6 +3,8 @@ vim.opt.relativenumber = true
 
 vim.opt.guicursor = ""
 
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -26,7 +28,7 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.diagnostic.config({ virtual_text = true })
 
