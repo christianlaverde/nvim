046ecf52c96566be1222f3d6f3a6afcbd1a6aa3a
Christian LaVerde <chrislaverde1@gmail.com>
2025-05-19 21:22:05 -0500

    Update telescope remaps

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -15,13 +15,11 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
     vim.keymap.set("n", "<leader>pf", builtin.git_files, { desc = "Telescope git files" })
-    vim.keymap.set("n", "<leader>ps", function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "Telescope live grep" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-
+    vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope lsp references" })
+    vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { desc = "Telescope lsp definitions" })
     vim.keymap.set("n", "<leader>en", function()
       builtin.find_files { cwd = vim.fn.stdpath "config" }
     end)
