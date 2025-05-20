eeb87a388f2c2a853d83b351764faba7eeb2f5c5
Christian LaVerde <chrislaverde1@gmail.com>
2025-05-19 21:20:58 -0500

    Add remaps for cnext and cpref for quickfix

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -10,3 +10,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- QuickFix
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>");
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>");
+
+
