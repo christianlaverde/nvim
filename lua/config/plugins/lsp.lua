9bff277f8e39db84ac76c4b283683cafc47fb375
Christian LaVerde <chrislaverde1@gmail.com>
2025-05-20 20:05:04 -0500

    more stuff :)

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -32,6 +32,7 @@ return {
         vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set('n', '<leader>vqd', function() vim.diagnostic.setqflist() end, opts)
         vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = 1, float = true}) end, opts)
         vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = -1, float = true}) end, opts)
         vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,6 +4,9 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- greatest keymap ever - via theprimeagen
+vim.keymap.set("v", "<leader>p", '\"_dP')
+
 -- vim.keymap.set("n", "J", "mzJ`c")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
